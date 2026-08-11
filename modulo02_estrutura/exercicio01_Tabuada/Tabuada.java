c86d4a91373733172da20cda4dd62d7299f96429
Rafa-Pilla <166242780+Rafa-Pilla@users.noreply.github.com>
2026-08-11 14:44:29 -0300

    Enhance multiplication table output format

--- modulo02_estrutura/exercicio01_Tabuada/Tabuada.java
+++ modulo02_estrutura/exercicio01_Tabuada/Tabuada.java
@@ -10,7 +10,7 @@ public class Tabuada {
 		int num = scanner.nextInt();
 			
 		for (int i=0; i < 11; i++) {
-			System.out.println(num * i);
+			System.out.println(i + " x " + num + " = " + (num * i));
 		}
     
         scanner.close();
