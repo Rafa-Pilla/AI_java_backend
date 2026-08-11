d4f8ce38f32f2d6f5f6a78009e406b08630c0a52
Rafa-Pilla <166242780+Rafa-Pilla@users.noreply.github.com>
2026-08-11 17:52:28 -0300

    Add files via upload

---
+++ Desafios/Desafio01_CriacaoConta/CriacaoConta.java
@@ -0,0 +1,25 @@
+package Desafios;
+import java.util.Scanner;
+
+public class CriacaoConta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] partes = input.split(" ");
+
+        String nome = partes[0];
+        String tipoConta = partes[1];
+
+        if (
+            tipoConta.equalsIgnoreCase("corrente") || 
+            tipoConta.equalsIgnoreCase("poupanca") || 
+            tipoConta.equalsIgnoreCase("investimento")) {
+                System.out.printf("Bem-vindo(a), %s! Sua conta %s está pronta para uso.", nome, tipoConta);
+        } else {
+            System.out.printf("Tipo de conta invalido.");
+        }
+
+        scanner.close();
+    }
+}
